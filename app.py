5bbb64b12752b56cbded26f96992593f23c2f702
MKFMIKU <mikumkf@outlook.com>
2017-01-31 21:58:59 +0800

    ADD App.py

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import time
+import os
+import sys
+
+# Clear the screen
+os.system('clear')
+
+print("\033[;32;m"+time.ctime()+"\n\033[0m")
+print("List function:\n")
+
+cmd = input("Your Cmmand:[   ]\b\b\b")
